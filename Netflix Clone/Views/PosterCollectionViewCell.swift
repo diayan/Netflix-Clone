accb6fed6ce8a3c4b1ea5303fc6c59f0af34e428
diayan <diayans@outlook.com>
2022-02-26 23:53:33 +0000

    create poster collection view cell to display movie posters

---
+++ Netflix Clone/Views/PosterCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by diayan siat on 26/02/2022.
+//
+
+import UIKit
+
+class PosterCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "PosterCollectionViewCell"
+    
+    private lazy var posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+}

2a95482f3deccf0aa6b4be7bfa09b1355ae35656
diayan <diayans@outlook.com>
2022-02-28 12:37:47 +0000

    implement search view controller

--- Netflix Clone/Controllers/SearchVC.swift
+++ Netflix Clone/Controllers/SearchVC.swift
@@ -8,10 +8,70 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    private var movies = [Movies]()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.showsVerticalScrollIndicator = false
+        tableView.register(UpcomingTableViewCell.self, forCellReuseIdentifier: UpcomingTableViewCell.identifier)
+        return tableView
+    }()
 
+    private lazy var searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultVC())
+        controller.searchBar.placeholder = "Search for a Movie or Tv show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.tintColor = .white
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        fetchDiscoverMovies()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func fetchDiscoverMovies(){
+        NetworkManager.shared.getDiscoverMovies { [weak self] results in
+            switch results {
+            case .success(let discover):
+                self?.movies = discover
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+extension SearchVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UpcomingTableViewCell.identifier, for: indexPath) as! UpcomingTableViewCell
+        let titleName = movies[indexPath.row].originalName ?? movies[indexPath.row].originalTitle ?? "Unknown"
+        cell.configure(with: PosterViewModel(titleName: titleName, posterUrl: movies[indexPath.row].posterPath ?? ""))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }

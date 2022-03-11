13a3fa7b48e4a347b714b53dc286a1a003960b48
diayan <diayans@outlook.com>
2022-03-11 12:17:02 +0000

    create a testing app delegate file

---
+++ Netflix Clone/TestingAppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  TestingAppDelegate.swift
+//  Netflix Clone
+//
+//  Created by diayan siat on 11/03/2022.
+//
+
+import Foundation
+import UIKit
+
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print(">> Launching with testing app delegate")
+        return true
+    }
+}
+
